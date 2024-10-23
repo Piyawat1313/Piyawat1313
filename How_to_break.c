deaf9fe8cfb7aaaab306298d8b3af93b75956a8b
Piyawat1313 <Piyawatbiwkai@gmail.com>
2024-10-23 12:55:17 +0700

    Create How_to_break.c

---
+++ How_to_break.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+void main() {
+      int x, i=0;
+      int sum = 0;
+
+    //1 true 0=false
+
+    while(1){
+        scanf("%d",&x);
+            if(x==0){
+                break;
+            }
+                sum+=1;
+
+
+            i++;
+
+    }
+      printf("%d",sum);
+}
